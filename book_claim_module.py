baf4a08d849603e2c23f498f79a1ebcc191f142a
stepanchizhov <stepan.chizhov@gmail.com>
2025-08-23 10:04:35 +0100

    Update book_claim_module.py

--- book_claim_module.py
+++ book_claim_module.py
@@ -1666,8 +1666,8 @@ class BookClaimModule:
             dict: Field data with name and value, or None if no promo should be shown
         """
         # Only show promotional messages every 2 commands (or if forced)
-        if not force_show and self.command_counter % 2 != 0:
-            return None
+        #if not force_show and self.command_counter % 2 != 0:
+        #    return None
         
         # Define all promotional messages
         promo_messages = [
