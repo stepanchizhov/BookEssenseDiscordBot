17ce118e43e40930c966c879f123231ba84cf9a0
stepanchizhov <stepan.chizhov@gmail.com>
2025-09-01 18:46:55 +0100

    Update book_claim_module.py

--- book_claim_module.py
+++ book_claim_module.py
@@ -1604,7 +1604,7 @@ class BookClaimModule:
         promo = promo_messages[promo_index]
         
         # Create the Patreon goal message
-        current_amount = 105  # Update this manually
+        current_amount = 114  # Update this manually
         goal_amount = 400
         percentage = (current_amount / goal_amount) * 100
         bar_length = 10
